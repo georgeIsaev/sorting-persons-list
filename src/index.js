b7d4fcb57f4c912edd085a194dbe631da9e41e8a
George <georgeisaev7@gmail.com>
2020-01-19 17:17:25 +0300

    Remake title and .ico

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+
+
+
 import './assets/main.scss';
 
 import {createControlPanel} from './js/view/controlPanel/createControlPanel';
 import {SortState} from './js/controller/SortState';
 import {addPersonsInDOM} from './js/controller/addPersonsInDOM';
 
-const app = document.getElementById('app');
+const app = document.getElementById('root');
 const sortState = new SortState(app);
 const controlPanel = createControlPanel();
 
